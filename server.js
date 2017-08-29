1239529892aa25db1161059f4df060252a03f905
guy kuperly <guyk22@gmail.com>
2017-08-29 22:50:32 +0300

    third deploy

--- server.js
+++ server.js
@@ -317,6 +317,6 @@ app.get('/api/getAllCashOut',function(req,res){
     
 });
 
-
-app.listen(3000);
+app.set('port', (process.env.PORT || 3000))
+app.listen(app.get('port'));
 console.log("Server running from port 3000");
