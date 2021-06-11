de27331a80ebb36e6b20d6b358dbb7e261615991
Guy <guy@fuseautotech.com>
2021-06-11 14:49:49 +0300

    #

--- public/Controllers/balance_controller.js
+++ public/Controllers/balance_controller.js
@@ -400,6 +400,7 @@
             var p = [];
             // remove empty players from array
             angular.forEach($scope.players,function(player){
+                debugger;
                 if((player.status.toLowerCase() == 'guest' && $scope.showGuests) ||  player.status.toLowerCase() != 'guest' &&  player.status.toLowerCase() == 'active' && player.totalCashIn != 0){
                     if(!player.availableMaxGames) {
                         player.availableMaxGames = getPlayerMaxAvalibleGames(player);
